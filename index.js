49b4348f94e6b3fd612c1196dfa8b62b7262c377
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-01-19 11:30:30 +0700

    a full code of a simple counter program

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const PI = 3.14159;
-let radius;
-let circumference;
+// COUNTER PROGARM
 
-document.getElementById("mySubmit").onclick = clicked = () => {
-    radius = document.getElementById("myText").value;
-    radius = Number(radius);
-    circumference = 2* PI * radius;
-    document.getElementById("myH3").textContent = `${circumference}cm`;
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = increase = () => {
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = decrease = () => {
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = reset = () => {
+    count = 0;
+    countLabel.textContent = count;
 }
