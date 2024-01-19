939be7675036ed2b5d22cd9e3cd6a47a2651d439
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-01-19 11:05:22 +0700

    Constant with HTML box input

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-// type conversion = change the datatype of a value to another
-//                  (strings, numbers, booleans)
+const PI = 3.14159;
+let radius;
+let circumference;
 
-let x = ``;
-let y = ``;
-let z = ``;
-
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-
-//converting string to boolean always true
-//0 also considered as true
-//but if empty literal or not defined var, its false then
-console.log(z, typeof z);
+document.getElementById("mySubmit").onclick = clicked = () => {
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2* PI * radius;
+    document.getElementById("myH3").textContent = `${circumference}cm`;
+}
