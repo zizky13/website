896e701b40edfe6978d7c6febded1ba24daca219
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-01-19 10:52:31 +0700

    Variety of type convertion

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // type conversion = change the datatype of a value to another
 //                  (strings, numbers, booleans)
 
-let age = window.prompt("How old are you?");
+let x = ``;
+let y = ``;
+let z = ``;
 
-age = Number(age);
-age += 1;
+x = Number(x);
+y = String(y);
+z = Boolean(z);
 
-console.log(age, typeof age);
+console.log(x, typeof x);
+console.log(y, typeof y);
+
+//converting string to boolean always true
+//0 also considered as true
+//but if empty literal or not defined var, its false then
+console.log(z, typeof z);
