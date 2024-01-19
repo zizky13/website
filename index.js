8c8133903595040b61c44c5abd39fcbad286346d
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-01-19 14:39:26 +0700

    adding checkbox, radio button, as well as method to reset selection

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-// RANDOM NUM GEN
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+const myReset = document.getElementById("myReset");
 
-const myButton = document.getElementById("myButton");
-const label1 = document.getElementById("label1");
-const label2 = document.getElementById("label2");
-const label3 = document.getElementById("label3");
-const min = 1;
-const max = 6;
-let randomNum1;
-let randomNum2;
-let randomNum3;
+mySubmit.onclick = submitClicked = () => {
+    if (myCheckBox.checked) {
+        subResult.textContent = `You are subscribed!`
+    } else {
+        subResult.textContent = `You are not subscribed!`
+    }
 
-myButton.onclick = clicked = () => {
-    randomNum1 = Math.floor(Math.random() * max) + min;
-    randomNum2 = Math.floor(Math.random() * max) + min;
-    randomNum3 = Math.floor(Math.random() * max) + min;
-    label1.textContent = randomNum1;
-    label2.textContent = randomNum2;
-    label3.textContent = randomNum3;
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa!`;
+    }
+    else if (masterCardBtn.checked) {
+        paymentResult.textContent = `You are paying with Master Card!`;
+    }
+    else if (payPalBtn.checked) {
+        paymentResult.textContent = `You are paying with PayPal!`;
+    } else {
+        paymentResult.textContent = `You must select a payment type!`;
+    }
+}
+
+myReset.onclick = resetClicked = () => {
+    if (visaBtn.checked || masterCardBtn.checked || payPalBtn.checked || myCheckBox.checked) {
+        document.getElementById("myCheckBox").checked = false;
+        document.getElementById("visaBtn").checked = false;
+        document.getElementById("masterCardBtn").checked = false;
+        document.getElementById("payPalBtn").checked = false;
+    }
 }
