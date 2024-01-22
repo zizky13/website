bf004dd3f60ab598cf38a3b91969dcf04da117aa
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-01-22 16:01:30 +0700

    source codes for random password generator using simple user interface

--- index.js
+++ index.js
@@ -1,18 +1,77 @@
-// rest parameter do the opposite of spread operator
+//random password generator
 
-rollDice = () => {
-    const numOfDice = document.getElementById("numOfDice").value; //catching value inputted by user in input HTML
-    const diceResult = document.getElementById("diceResult"); //catching value of current dice result
-    const diceImages = document.getElementById("diceImages"); //catching value of current dice image
-    const values = []; //array to hold randomized value
-    const images = []; //array to hold dice face images
+let passwordLength = 0;
+let includeLowercase = false;
+let includeUppercase = false;
+let includeNumbers = false;
+let includeSymbols = false;
 
-    for(let i = 0; i < numOfDice; i++){//iterating by number of dices inputted by user
-        const value = Math.floor(Math.random() * 6) + 1; //randomize number from 1 to 6, save it to value
-        values.push(value); //pushing the randomized number to the array
-        images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`); //pushing the image to array according to the value resulted
+generateCheck = () => {
+    passwordLength = document.getElementById("passwordLength").value;
+
+    if (useLowercaseCheck.checked) {
+        includeLowercase = true;
+    } else {
+        includeLowercase = false;
     }
 
-    diceResult.textContent = `dice:${values.join(', ')}`; //printing dice result according to value received
-    diceImages.innerHTML = images.join(''); //printing the images
+    if (useUppercaseCheck.checked) {
+        includeUppercase = true;
+    } else {
+        includeUppercase = false;
+    }
+
+    if (useNumbersCheck.checked) {
+        includeNumbers = true;
+    } else {
+        includeNumbers = false;
+    }
+
+    if (useSymbolsCheck.checked) {
+        includeSymbols = true;
+    } else {
+        includeSymbols = false;
+    }
+
+    generatePassword = (length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) => {
+        const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const numberChars = "0123456789";
+        const symbolsChars = "!@#$%^&*()_+-=[]\;',./{}|:<>?";
+
+        let allowedChars = "";
+        let password = "";
+
+
+        allowedChars += includeLowercase ? lowercaseChars : "";
+        allowedChars += includeUppercase ? uppercaseChars : "";
+        allowedChars += includeNumbers ? numberChars : "";
+        allowedChars += includeSymbols ? symbolsChars : "";
+
+        if (length <= 0) {
+            return '(password length must be at least 1';
+        }
+
+        if (allowedChars.length === 0) {
+            return `(Atleast 1 set of characters needs to be selected)`;
+        }
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * allowedChars.length);
+            password += allowedChars[randomIndex];
+
+        }
+
+        return password;
+    }
+
+    const password = generatePassword(passwordLength,includeLowercase,includeUppercase,includeNumbers,includeSymbols);
+    generatedPassword.textContent = `Your generated password is: ${password}`;
+
 }
+
+
+
+
+
+
