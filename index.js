93d7974be27525b76cef038109c0549a2b3abd5c
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-01-22 13:47:35 +0700

    changing some typos and adding comments

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // rest parameter do the opposite of spread operator
 
 rollDice = () => {
-    const numOfDice = document.getElementById("numOfDice").value;
-    const diceResult = document.getElementById("diceResult");
-    const diceImages = document.getElementById("diceImages");
-    const values = [];
-    const images = [];
+    const numOfDice = document.getElementById("numOfDice").value; //catching value inputted by user in input HTML
+    const diceResult = document.getElementById("diceResult"); //catching value of current dice result
+    const diceImages = document.getElementById("diceImages"); //catching value of current dice image
+    const values = []; //array to hold randomized value
+    const images = []; //array to hold dice face images
 
-    for(let i = 0; i < numOfDice; i++){
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`)
+    for(let i = 0; i < numOfDice; i++){//iterating by number of dices inputted by user
+        const value = Math.floor(Math.random() * 6) + 1; //randomize number from 1 to 6, save it to value
+        values.push(value); //pushing the randomized number to the array
+        images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`); //pushing the image to array according to the value resulted
     }
 
-    diceResult.textContent = `dice:${values.join(', ')}`;
-    diceImages.innerHTML = images.join('')
+    diceResult.textContent = `dice:${values.join(', ')}`; //printing dice result according to value received
+    diceImages.innerHTML = images.join(''); //printing the images
 }
