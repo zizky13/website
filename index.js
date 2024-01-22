43d38b506f4dbe6f0252d102ab33fcfc1dd94d2b
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-01-22 11:37:52 +0700

    more catchy version of rolling dice

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 // rest parameter do the opposite of spread operator
 
-openArmory = (...weapons) => {
-    console.log(...weapons);
+rollDice = () => {
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`)
+    }
+
+    diceResult.textContent = `dice:${values.join(', ')}`;
+    diceImages.innerHTML = images.join('')
 }
-
-const weapon1 = "knife";
-const weapon2 = "sword";
-const weapon3 = "rifle";
-const weapon4 = "magic orb";
-const weapon5 = "broadsword";
-
-openArmory(weapon1,weapon2,weapon3,weapon4,weapon5);
-// you can use rest to combine strings
-
-combineStrings = (...strings) => {
-    return strings.join(" ");
-}
-
-const fullName = combineStrings("Mr.", "Spongebob","Squarepants","III");
-console.log(fullName);
