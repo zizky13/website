18a86b885bcf2c48672e52921cbd95a0fd4de192
Zikar Nurizky <zikar.nurizky@gmail.com>
2024-01-22 11:08:46 +0700

    some examples of rest parameter usage

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-// spread operator = ... allows an iterable such as an
-//                   array or string to be expanded into
-//                   seperate elements (unpack the elements)
+// rest parameter do the opposite of spread operator
 
-let numbers = [1,2,3,4,5];
-let maximum = Math.max(...numbers);
-let minimum = Math.min(...numbers);
-let username = "Hokkaido"
-let letters = [...username].join("-");
-let fruits = ["apple","orange","banana"];
- //shallow copy of fruits
-let vegetables = ["carrots","celery","potato"]
-let foods = [...fruits, ...vegetables,"eggs","milk"];
+openArmory = (...weapons) => {
+    console.log(...weapons);
+}
 
+const weapon1 = "knife";
+const weapon2 = "sword";
+const weapon3 = "rifle";
+const weapon4 = "magic orb";
+const weapon5 = "broadsword";
 
-console.log(fruits);
-console.log(foods);
-console.log(letters);
-console.log(maximum);
-console.log(minimum);
+openArmory(weapon1,weapon2,weapon3,weapon4,weapon5);
+// you can use rest to combine strings
+
+combineStrings = (...strings) => {
+    return strings.join(" ");
+}
+
+const fullName = combineStrings("Mr.", "Spongebob","Squarepants","III");
+console.log(fullName);
